787828a1754c678a8d0cd474aa149dd2ad4e78ca
Momen <momenmostafa203@gmail.com>
2024-09-25 11:03:35 +0300

    handle logic of unverified user

--- backend/src/login/login.controller.ts
+++ backend/src/login/login.controller.ts
@@ -17,8 +17,18 @@ export const loginController = async (req: Request, res: Response) => {
     if (!isValid) {
         return res.status(401).send("Invalid name or password");
     }
+    if (!user.isVerified){
+        return res.status(401).send("User not verified");
+    }
 
     const token: string = createJWT(user);
+
+    await prisma.user.update({
+        where: {id: user.id},
+        data: {token: token}
+    });
+
+
     return res.status(200).json({
         msg: "User logged in successfully",
         data: {
