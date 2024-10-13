324c0d34d2698dd5370d47ba72aa1181ccb0053b
Momen <momenmostafa203@gmail.com>
2024-10-13 20:42:52 +0300

    query unsafe in sql injection

--- backend/src/labs/sqlInjectionLabs/hiddenData.controller.ts
+++ backend/src/labs/sqlInjectionLabs/hiddenData.controller.ts
@@ -1,14 +1,15 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import prisma from "../../utilities/db";
 
 export const findUser = async (req: Request, res: Response) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    // Secure parameterized query
-    const user: any = await prisma.$queryRaw`SELECT * 
-                                      FROM "userForSQLInjection" 
-                                      WHERE username = ${username} AND password = ${password}`;
+const user: any = await prisma.$executeRawUnsafe(`
+    SELECT * 
+    FROM "userForSQLInjection" 
+    WHERE username = '${username}' AND password = '${password}';
+`);
 
     if (user.length === 0) {
         return res.status(404).send("Not a user");
