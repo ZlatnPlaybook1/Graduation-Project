08616692923046eddf376d7ef374fe96baae1b72
Momen <momenmostafa203@gmail.com>
2024-11-26 18:07:02 +0200

    lab3 unrestrictedFileUpload

---
+++ backend/src/labs/unrestrictedFileUpload/lab3/lab3.controller.ts
@@ -0,0 +1,47 @@
+import multer from 'multer';
+import prisma from "../../../utilities/db";
+
+export const upload = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, 'uploads/'); // Directory where files will be stored
+        },
+        filename: (req, file, cb) => {
+            cb(null, `${Date.now()}-${file.originalname}`); // Unique filename
+        },
+    }),
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype === 'application/x-httpd-php') {
+            return cb(new Error('PHP files are not allowed'), false);
+        }
+        cb(null, true);
+    },
+});
+
+export async function saveFile(req, res)  {
+  try {
+    const file = req.file;
+
+    if (!file) {
+      return res.status(400).send('No file uploaded.');
+    }
+
+    // Save file details to the database
+    const savedFile = await prisma.file.create({
+      data: {
+        name: file.originalname,
+        mimetype: file.mimetype,
+        path: file.path,
+        userId:req.params.id
+      },
+    });
+
+    res.status(200).json({
+      message: 'File uploaded successfully',
+      file: savedFile,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error uploading file.');
+  }
+}
