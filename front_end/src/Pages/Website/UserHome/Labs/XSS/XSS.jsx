aad66c71c54cca3dc1d52895115e2c50d427871f
Ahmed Sherif <as4349124@gmail.com>
2024-12-04 21:34:17 +0200

    Complete 3th Lab IDOR  successfully

--- front_end/src/Pages/Website/UserHome/Labs/XSS/XSS.jsx
+++ front_end/src/Pages/Website/UserHome/Labs/XSS/XSS.jsx
@@ -51,7 +51,7 @@ export default function XSS() {
           <div className="breadcrumb-lab2">
             <ul>
               <li>
-                <Link to="">Home</Link>
+                <Link to="/home">Home</Link>
               </li>
               <li>
                 <Link to="">Cross-Site Scripting</Link>
