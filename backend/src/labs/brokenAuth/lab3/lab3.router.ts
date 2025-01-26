b61472e957d578fc6f815b96e86b961cfa26abcb
Momen <momenmostafa203@gmail.com>
2025-01-26 15:11:27 +0200

    fix bug

--- backend/src/labs/brokenAuth/lab3/lab3.router.ts
+++ backend/src/labs/brokenAuth/lab3/lab3.router.ts
@@ -3,7 +3,7 @@ import {login} from "./lab3.controller";
 
 let lab3BrokenAuthRouter = Router();
 
-lab3BrokenAuthRouter.post("/brokenAuthLab2", login);
+lab3BrokenAuthRouter.post("/brokenAuthLab3", login);
 
 
 export default lab3BrokenAuthRouter;
