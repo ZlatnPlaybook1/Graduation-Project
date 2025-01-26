6bcc1f6f084cfa39927dbafd250f7bcbf526695c
Momen <momenmostafa203@gmail.com>
2025-01-26 14:09:01 +0200

    rename email to username in lab1,2 brokenAuth

--- backend/src/labs/brokenAuth/lab1/lab1.controller.ts
+++ backend/src/labs/brokenAuth/lab1/lab1.controller.ts
@@ -2,14 +2,14 @@ import {Request, Response} from "express";
 
 export const login = async (req: Request, res: Response) => {
     try {
-        const email = "admin";
+        const username = "admin";
         const password = "Cyberlab";
 
-        if (!req.body.email || !req.body.password) {
+        if (!req.body.username || !req.body.password) {
             return res.status(400).send("Email and password are required");
         }
-        if (req.body.email === email && req.body.password === password) {
-            return res.status(200).json({role:"support"})
+        if (req.body.username === username && req.body.password === password) {
+            return res.status(200).json({message:"success"})
         }
     }
     catch (e) {
