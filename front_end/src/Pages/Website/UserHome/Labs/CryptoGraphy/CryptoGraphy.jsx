eecaf897f1d37dd48f2fe8393c14d4f4f593829e
ebrahim-hima <nasar.39305041@gmail.com>
2025-01-27 16:16:26 +0200

    CryptoGraphy content  update

--- front_end/src/Pages/Website/UserHome/Labs/CryptoGraphy/CryptoGraphy.jsx
+++ front_end/src/Pages/Website/UserHome/Labs/CryptoGraphy/CryptoGraphy.jsx
@@ -9,6 +9,13 @@ import { Link } from "react-router-dom";
 import Footer from "../../Footer/Footer";
 import exampleImage1 from "../../assets/img/Cryptography/1.png";
 import exampleImage2 from "../../assets/img/Cryptography/2.png";
+import exampleImage3 from "../../assets/img/Cryptography/3.svg";
+import exampleImage4 from "../../assets/img/Cryptography/4.svg";
+import exampleImage5 from "../../assets/img/Cryptography/5.svg";
+import exampleImage6 from "../../assets/img/Cryptography/6.svg";
+import exampleImage7 from "../../assets/img/Cryptography/7.svg";
+import exampleImage8 from "../../assets/img/Cryptography/8.svg";
+import exampleImage9 from "../../assets/img/Cryptography/9.svg";
 
 
 
@@ -215,6 +222,121 @@ export default function CryptoGraphy() {
                 <p>Steghide tool to encrypt and hide data.</p>
                 <br></br>
                 </dd> 
+
+
+                <dt className="wave-lab1 fadeInUp faq-header-lab1">
+                  <span> step 4</span> Plaintext to Ciphertext </dt>
+                  <dd className="fadeInUp faq-body-lab1" id="border-left">
+                <br></br>
+                <p>Let’s start with an illustration before introducing the key terms. We begin with the plaintext that we want to encrypt. The plaintext is the readable data; it can be anything from a simple “hello”, a cat photo, credit card information, or medical health records. From a cryptography perspective, these are all “plaintext” messages waiting to be encrypted. The plaintext is passed through the encryption function along with a proper key; the encryption function returns a ciphertext. The encryption function is part of the cipher;
+                  a cipher is an algorithm to convert a plaintext into a ciphertext and vice versa.</p>
+                <br></br>
+                <img id="img-full" src={exampleImage3} alt="Example" />
+                <br></br>
+                <p>To recover the plaintext, we must pass the ciphertext along with the proper key via the decryption function, which would give us the original plaintext. This is shown in the illustration below.</p>
+                <br></br>
+                <img id="img-full" src={exampleImage4} alt="Example" />
+                <br></br>
+                <p>We have just introduced several new terms, and we need to learn them to understand any text about cryptography. The terms are listed below:</p>
+                <ul>
+                <li><strong>Plaintext</strong> is the original, readable message or data before it’s encrypted. It can be a document, an image, a multimedia file, or any other binary data.</li>
+                <li><strong>Ciphertext</strong> is the scrambled, unreadable version of the message after encryption. Ideally, we cannot get any information about the original plaintext except its approximate size.</li>
+                <li><strong>Cipher</strong> is an algorithm or method to convert plaintext into ciphertext and back again. A cipher is usually developed by a mathematician.</li>
+                <li><strong>Key</strong> is a string of bits the cipher uses to encrypt or decrypt data. In general, the used cipher is public knowledge; however, the key must remain secret unless it is the public key in asymmetric encryption. We will visit asymmetric encryption in a later task.</li>
+                <li><strong>Encryption</strong> is the process of converting plaintext into ciphertext using a cipher and a key. Unlike the key, the choice of the cipher is disclosed.</li>
+                <li><strong>Decryption</strong> is the reverse process of encryption, converting ciphertext back into plaintext using a cipher and a key. Although the cipher would be public knowledge, recovering the plaintext without knowledge of the key should be impossible (infeasible).</li>
+                </ul>
+
+            </dd>
+
+
+            <dt className="wave-lab1 fadeInUp faq-header-lab1">
+                  <span> step 5</span> Historical Ciphers </dt>
+                  <dd className="fadeInUp faq-body-lab1" id="border-left">
+                <p><br></br></p>
+
+                <p>Cryptography’s history is long and dates back to ancient Egypt in 1900 BCE. However, one of the simplest historical ciphers is the Caesar Cipher from the first century BCE. The idea is simple: shift each letter by a certain number to encrypt the message.</p>
+                <p>Consider the following example:</p>
+                <ul>
+                <li>Plaintext: <code>cyber lab</code></li>
+                <li>Key: 3 (Assume it is a right shift of 3.)</li>
+                <li>Cipher: Caesar Cipher</li>
+                </ul>
+                <p>We can easily figure out that c becomes f, y becomes b, b becomes e, and so on. As you noticed, once we reach Z, we start all over, as shown in the figure below. Consequently, we get the ciphertext of <code>fbehu ode</code>.</p>
+                <br></br>
+                <img id="img-full" src={exampleImage5} alt="Example" />
+                <br></br>
+                <p>To decrypt, we need the following information:</p>
+                <ul>
+                <li>Ciphertext: <code>fbehu ode</code></li>
+                <li>Key: 3</li>
+                <li>Cipher: Caesar Cipher</li>
+                </ul>
+                <br></br>
+                <img id="img-full" src={exampleImage6} alt="Example" />
+                <br></br>
+                <p>For encryption, we shift to the right by three; for decryption, we shift to the left by three and recover the original plaintext, as illustrated in the image above. However, if someone gives you a ciphertext and tells you that it was encrypted using Caesar Cipher, recovering the original text would be a trivial task as there are only 25 possible keys. The English alphabet is 26 letters, and shifting by 26 will keep the letter unchanged; hence, 25 valid keys for encryption with Caesar Cipher. The figure below shows how decryption will succeed by attempting all the possible keys; in this case, we recovered the original message with <span class="math inline"><em>K</em><em>e</em><em>y</em> = 5</span>. Consequently, by today’s standards, where the cipher is publicly known, Caesar Cipher is considered insecure.</p>
+                <br></br>
+                <img id="img-full" src={exampleImage7} alt="Example" />
+                <br></br>
+
+                <p>You would come across many more historical ciphers in movies and cryptography books. Examples include:</p>
+                <ul>
+                <li>The Vigenère cipher from the 16th century</li>
+                <li>The Enigma machine from World War II</li>
+                <li>The one-time pad from the Cold War</li>
+                </ul>
+                </dd> 
+
+                <dt className="wave-lab1 fadeInUp faq-header-lab1">
+                  <span> step 6</span> Types of Encryption </dt>
+                  <dd className="fadeInUp faq-body-lab1" id="border-left">
+                <p><br></br></p>
+
+                <h3>The two main categories of encryption are <strong>symmetric</strong> and <strong>asymmetric</strong>.</h3>
+                <br></br>
+                <h3>Symmetric Encryption : </h3>
+                <br></br>
+                <p><strong>Symmetric encryption</strong>, also known as <strong>symmetric cryptography</strong>, uses the same key to encrypt and decrypt the data, as shown in the figure below. Keeping the key secret is a must; it is also called <strong>private key cryptography</strong>. Furthermore, communicating the key to the intended parties can be challenging as it requires a secure communication channel. Maintaining the secrecy of the key can be a significant challenge, especially if there are many recipients. The problem becomes more severe in the presence of a powerful adversary; consider the threat of industrial espionage, for instance.</p>
+
+                <img id="img-full" src={exampleImage8} alt="Example" />
+                <p>Consider the simple case where you created a password-protected document to share it with your colleague. You can easily email the encrypted document to your colleague, but most likely, you cannot email them the password. The reason is that anyone with access to their mailbox would access both the password-protected document and its password. Therefore, you need to think of a different way, i.e., channel, to share the password. Unless you think of a secure, accessible channel, one solution would be to meet in person and communicate the password to them.</p>
+                <p>Examples of symmetric encryption are <strong >DES</strong> (Data Encryption Standard), 3DES (Triple <strong >DES</strong>) and <strong >AES</strong> (Advanced Encryption Standard).</p>
+                <br></br>
+                <ul>
+                <li><strong>DES</strong> was adopted as a standard in 1977 and uses a 56-bit key. With the advancement in computing power, in 1999, a <strong >DES</strong> key was successfully broken in less than 24 hours, motivating the shift to 3DES.</li>
+                <br></br>
+                <li><strong>3DES</strong> is DES applied three times; consequently, the key size is 168 bits, though the effective security is 112 bits. 3DES was more of an <strong >ad</strong>-hoc solution when <strong >DES</strong> was no longer considered secure. 3DES was deprecated in 2019 and should be replaced by <strong >AES</strong>; however, it may still be found in some legacy systems.</li>
+                <br></br>
+                <li><strong>AES</strong> was adopted as a standard in 2001. Its key size can be 128, 192, or 256 bits.</li>
+                </ul>
+                <p>There are many more symmetric encryption ciphers used in various applications; however, they have not been adopted as standards.</p>
+                <br></br>
+                <h2>Asymmetric Encryption</h2>
+                <br></br>
+                <p>Unlike symmetric encryption, which uses the same key for encryption and decryption, <strong>asymmetric encryption</strong> uses a pair of keys, one to encrypt and the other to decrypt, as shown in the illustration below. To protect confidentiality, asymmetric encryption or <strong>asymmetric cryptography</strong> encrypts the data using the public key; hence, it is also called <strong>public key cryptography</strong>.</p>
+                <br></br>
+                <img id="img-full" src={exampleImage9} alt="Example" />
+                <br></br>
+                <p>Examples are RSA , Diffie-Hellman, and Elliptic Curve cryptography (<span >ECC</span>). The two keys involved in the process are referred to as a <b>public key</b> and a <b>private key</b>. Data encrypted with the public key can be decrypted with the private key. Your private key needs to be kept private, hence the name.</p>
+                <p>Asymmetric encryption tends to be slower, and many asymmetric encryption ciphers use larger keys than symmetric encryption. For example, <span >RSA</span> uses 2048-bit, 3072-bit, and 4096-bit keys; 2048-bit is the recommended minimum key size. Diffie-Hellman also has a recommended minimum key size of 2048 bits but uses 3072-bit and 4096-bit keys for enhanced security. On the other hand, <span >ECC</span> can achieve equivalent security with shorter keys. For example, with a 256-bit key, <span >ECC</span> provides a level of security comparable to a 3072-bit <span >RSA</span> key.</p>
+                <p>Asymmetric encryption is based on a particular group of mathematical problems that are easy to compute in one direction but extremely difficult to reverse. In this context, extremely difficult means practically infeasible. For example, we can rely on a mathematical problem that would take a very long time, for example, millions of years, to solve using today’s technology.</p>
+                <p>We will visit various asymmetric encryption ciphers in the next room. For now, the important thing to note is that asymmetric encryption provides you with a public key that you share with everyone and a private key that you keep guarded and secret.</p>
+                <br></br>
+                <h2>Summary of New Terms :</h2>
+                <br></br>
+                <ul>
+                <li><strong>Alice and Bob</strong> are fictional characters commonly used in cryptography examples to represent two parties trying to communicate securely.</li>
+                <li><strong>Symmetric encryption</strong> is a method in which the same key is used for both encryption and decryption. Consequently, this key must remain secure and never be disclosed to anyone except the intended party.</li>
+                <li><strong>Asymmetric encryption</strong> is a method that uses two different keys: a public key for encryption and a private key for decryption.</li>
+                </ul>
+
+
+            </dd> 
+
+
+
+
 
               
                 {/* 
