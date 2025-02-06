54cef1d15b8095f0e51eaca46249539bb3f41df3
Momen <momenmostafa203@gmail.com>
2025-02-06 10:16:35 +0200

    lab3 burpsuite

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -38,6 +38,7 @@ import lab3CSRF from "./labs/CSRF/lab3/lab3.router";
 import lab1fileInclusion from "./labs/fileInclusion/lab1/lab1.router";
 import lab2fileInclusion from "./labs/fileInclusion/lab2/lab2.router";
 import lab3fileInclusion from "./labs/fileInclusion/lab3/lab3.router";
+import BurPSuiteLab3 from "./labs/burpsuite/lab3/lab3.router"; 
 
 const app = express();
 app.use("/uploads", express.static("uploads"));
@@ -93,6 +94,7 @@ app.use("/api", lab3CSRF);
 app.use("/api", lab1fileInclusion);
 app.use("/api", lab2fileInclusion);
 app.use("/api", lab3fileInclusion);
+app.use("/api", BurPSuiteLab3);
 
 dotenv.config();
 
