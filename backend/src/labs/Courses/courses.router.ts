5b6b691964134e86846f8950d661c59805db4f8c
Momen <momenmostafa203@gmail.com>
2025-02-07 07:12:26 +0200

    migrate courses

--- backend/src/labs/Courses/courses.router.ts
+++ backend/src/labs/Courses/courses.router.ts
@@ -1,14 +1,14 @@
-// // router.ts
+// router.ts
 
-// import express from "express";
-// import { updateFavorite, registerCourse } from "./courses.controller";
+import express from "express";
+import { updateFavorite, registerCourse } from "./courses.controller";
 
-// const courses = express.Router();
+const courses = express.Router();
 
-// // Endpoint for updating favorite status.
-// courses.post("/favorite", updateFavorite);
+// Endpoint for updating favorite status.
+courses.post("/favorite", updateFavorite);
 
-// // Endpoint for course registration.
-// courses.post("/register", registerCourse);
+// Endpoint for course registration.
+courses.post("/register", registerCourse);
 
-// export default courses;
+export default courses;
