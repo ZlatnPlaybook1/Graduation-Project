53db481523a75029e44ef97472ca2e541178c8b8
Momen <momenmostafa203@gmail.com>
2025-02-10 14:39:31 +0200

    make a delay in lab 1 race condition

--- backend/src/labs/raceCondition/lab1/lab1.controller.ts
+++ backend/src/labs/raceCondition/lab1/lab1.controller.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
 import prisma from "../../../utilities/db";
 
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
 export const register = async (req: Request, res: Response) => {
     try {
+        await delay(300); // 0.3 second delay
 
         const user = await prisma.lab1RaceCondition.findUnique({
             where: {
@@ -17,7 +20,7 @@ export const register = async (req: Request, res: Response) => {
                 email: req.body.email,
                 name: req.body.name,
                 surname: req.body.surname,
-                tel : req.body.tel,
+                tel: req.body.tel,
             }
         });
         return res.status(201).json({
@@ -25,9 +28,9 @@ export const register = async (req: Request, res: Response) => {
         });
     } catch (e) {
         console.error(e);
+        return res.status(500).send("Internal Server Error");
     }
-    return res.status(500).send("Internal Server Error");
-}
+};
 
 export const viewRegister = async (req: Request, res: Response) => {
     try {
@@ -35,9 +38,9 @@ export const viewRegister = async (req: Request, res: Response) => {
         return res.status(200).json(users);
     } catch (e) {
         console.error(e);
+        return res.status(500).send("Internal Server Error");
     }
-    return res.status(500).send("Internal Server Error");
-}
+};
 
 export const deleteRegister = async (req: Request, res: Response) => {
     try {
@@ -57,6 +60,6 @@ export const deleteRegister = async (req: Request, res: Response) => {
         return res.status(200).send("User deleted successfully");
     } catch (e) {
         console.error(e);
+        return res.status(500).send("Internal Server Error");
     }
-    return res.status(500).send("Internal Server Error");
-}
+};
