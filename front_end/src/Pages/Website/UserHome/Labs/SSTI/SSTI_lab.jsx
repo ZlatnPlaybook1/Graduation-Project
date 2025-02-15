7e7800b6a264f324bc133452fc58d0877e799456
ebrahiemdelh <ebrahiemgamal@gmail.com>
2025-02-15 22:22:54 +0200

    edit

--- front_end/src/Pages/Website/UserHome/Labs/SSTI/SSTI_lab.jsx
+++ front_end/src/Pages/Website/UserHome/Labs/SSTI/SSTI_lab.jsx
@@ -52,7 +52,7 @@ export default function SSTI_lab() {
             difficulty={"Easy"}
             />
             <Card
-            link={"/SSTI/SSTI_lab2/store"}
+            link={"/SSTI/SSTIlab2/store"}
             image={image}
             title={"SSTI Store Vulnerability"}
             brief={"This lab is vulnerable to server-side template injection due to the unsafe construction of an ERB template."}
