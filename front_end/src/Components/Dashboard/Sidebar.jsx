207a276bebfd4b62a9057e1f7ba8b1969af6eac3
Eng-Ahmed-Hussien <Ahmed.hussien685@el-eng.menofia.edu.eg>
2025-02-24 13:06:56 +0200

    .

--- front_end/src/Components/Dashboard/Sidebar.jsx
+++ front_end/src/Components/Dashboard/Sidebar.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from "react";
-import "./bars.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFaceSmile,
--- front_end/src/Components/Dashboard/Topbar.jsx
+++ front_end/src/Components/Dashboard/Topbar.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from "react";
-import "./bars.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { Menu } from "../../Context/MenuContext";
