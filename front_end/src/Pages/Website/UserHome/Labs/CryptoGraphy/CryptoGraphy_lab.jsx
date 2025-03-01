ee42a7b782a04fd726d8fdca1ee63c33ec313347
ebrahiemdelh <ebrahiemgamal@gmail.com>
2025-03-01 23:04:13 +0200

    changed landing in ebrahiem's labs

--- front_end/src/Pages/Website/UserHome/Labs/CryptoGraphy/CryptoGraphy_lab.jsx
+++ front_end/src/Pages/Website/UserHome/Labs/CryptoGraphy/CryptoGraphy_lab.jsx
@@ -4,37 +4,14 @@ import Header from "../../Header/Header";
 import image from "../../assets/img/Cryptography/card_image.png";
 import Footer from "../../Footer/Footer";
 import { Card } from "../../Components/Card/Card";
+import LandingPractice from "../../Components/PracticeLanding/PracticeLanding";
+
 export default function CryptoGraphy_lab() {
   return (
     <>
       <Header />
       {/* Start Landing  */}
-      <div className="landing">
-        <div className="container">
-          <div className="course-info">
-            <div className="course-text">
-              <h1>Practice</h1>
-              <p className="brief">
-                <i className="fa-solid fa-bullseye"></i> Reinforce what you're
-                learning
-              </p>
-              <p>
-                Put your knowledge into practice with gamified cyber security
-                challenges.
-              </p>
-            </div>
-            <div className="course-icon">
-              <div className="border-icon">
-                <div className="content">
-                  <i className="fas fa-book-open"></i>
-                  <p>Practice</p>
-                  <i className="fas fa-bullseye"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <LandingPractice/>
       {/* End Landing  */}
       {/* Start Courses  */}
       <div className="course">
--- front_end/src/Pages/Website/UserHome/Labs/Linux/Linux_lab.jsx
+++ front_end/src/Pages/Website/UserHome/Labs/Linux/Linux_lab.jsx
@@ -4,6 +4,7 @@ import Header from "../../Header/Header";
 import image from "../../assets/img/linux/card_image.png";
 import Footer from "../../Footer/Footer";
 import { Card } from "../../Components/Card/Card";
+import LandingPractice from "../../Components/PracticeLanding/PracticeLanding";
 
 export default function Linux_lab() {
   useEffect(() => {
@@ -13,32 +14,7 @@ export default function Linux_lab() {
     <>
       <Header />
       {/* Start Landing  */}
-      <div className="landing">
-        <div className="container">
-          <div className="course-info">
-            <div className="course-text">
-              <h1>Practice</h1>
-              <p className="brief">
-                <i className="fa-solid fa-bullseye"></i> Reinforce what you're
-                learning
-              </p>
-              <p>
-                Put your knowledge into practice with gamified cyber security
-                challenges.
-              </p>
-            </div>
-            <div className="course-icon">
-              <div className="border-icon">
-                <div className="content">
-                  <i className="fas fa-book-open"></i>
-                  <p>Practice</p>
-                  <i className="fas fa-bullseye"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <LandingPractice/>
       {/* End Landing  */}
       {/* Start Courses  */}
       <div className="course">
