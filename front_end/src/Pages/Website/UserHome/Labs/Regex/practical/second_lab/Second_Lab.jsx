d0d98bc3cf29c0b6831c2c43b3446c865dfbf8eb
Eng-Ahmed-Hussien <Ahmed.hussien685@el-eng.menofia.edu.eg>
2025-03-12 07:30:41 +0200

    Regex Finished

--- front_end/src/Pages/Website/UserHome/Labs/Regex/practical/second_lab/Second_Lab.jsx
+++ front_end/src/Pages/Website/UserHome/Labs/Regex/practical/second_lab/Second_Lab.jsx
@@ -1,9 +1,11 @@
-import CompleteQuestion from "./CompleteQuestion";
+import CompleteQuestion from "../../../../Components/CompleteQuestion/CompleteQuestion";
 import Data from "./second_Data.json";
+import GoBackBtn from "../../../../Components/GoBack_Btn/GoBack_Btn";
 
 export default function Second_Lab() {
   return (
-    <div style={{ padding: "20px" }}>
+    <div >
+        <GoBackBtn />
       <CompleteQuestion questionsData={Data} />
     </div>
   );
